b033722497fdb9ac84d052cd86cb91c9eda7dbc1
Bocmen <Bocmen@users.noreply.github.com>
2020-08-19 23:59:53 +0300

    Реализована рабочая версия строкового калькулятора

---
+++ Project Visual Studio/Calculation Line/Calculator/Calculator.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Calculator
+{
+    public static class Calculator
+    {
+        private static Operator[] Operators = new Operator[]
+        {
+            new Operator { Symbol = ')', Priority = 0 },
+            new Operator { Symbol = '(', Priority = 0 },
+            new Operator { Symbol = '+', Priority = 1 },
+            new Operator { Symbol = '-', Priority = 1 },
+            new Operator { Symbol = '/', Priority = 2 },
+            new Operator { Symbol = '*', Priority = 2 },
+            new Operator { Symbol = '^', Priority = 3 }
+        };
+        public static double Calculate(string str)
+        {
+            try
+            {
+                object[] Elements = HendlerObject(str.Replace('.', ',').Replace("+-", "-").Replace("-+", "-"));
+                Stack<double> Number = new Stack<double>();
+                Stack<Operator> operators = new Stack<Operator>();
+                foreach (var elem in Elements)
+                {
+                    // Число ли это
+                    if (elem.GetType() == typeof(double))
+                    {
+                        Number.Push((double)elem);
+                    }
+                    else
+                    {
+                        // Если предыдущий оператор по приоритету меньше или равен
+                        if (operators.Count > 0 && Number.Count > 1 && operators.Peek().Priority != 0 && ((Operator)elem).Priority != 0 && operators.Peek().Priority >= ((Operator)elem).Priority)
+                        {
+                            Number.Push(CalculateTwoElem(Number.Pop(), Number.Pop(), operators.Pop()));
+                        }
+                        if (((Operator)elem).Priority == 0 && ((Operator)elem).Symbol == ')')
+                        {
+                            while (operators.Peek().Symbol != '(')
+                                Number.Push(CalculateTwoElem(Number.Pop(), Number.Pop(), operators.Pop()));
+                            operators.Pop();
+                        }
+                        else
+                            operators.Push((Operator)elem);
+                    }
+                }
+                for (int i = operators.Count; i > 0; i--)
+                    Number.Push(CalculateTwoElem(Number.Pop(), Number.Pop(), operators.Pop()));
+                return Number.Pop();
+            }
+            catch
+            {
+                throw new Exception("Введенное выражение неправильно записано");
+            }
+        }
+        private static double CalculateTwoElem(double Two, double One, Operator @operator)
+        {
+            switch (@operator.Symbol)
+            {
+                case '+':
+                    return One + Two;
+                case '-':
+                    return One - Two;
+                case '*':
+                    return One * Two;
+                case '/':
+                    return One / Two;
+                case '^':
+                    return Math.Pow(One, Two);
+                default:
+                    return 0;
+            }
+        }
+        private static object[] HendlerObject(string str)
+        {
+            List<object> list = new List<object>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                string rS = null;
+                while (i < str.Length && (str[i] == ',' || double.TryParse(str[i].ToString(), out double r)))
+                    rS += str[i++];
+                if (rS != null)
+                {
+                    i--;
+                    list.Add(double.Parse(rS));
+                }
+                else
+                    list.Add(Operators.First(x => x.Symbol == str[i]));
+            }
+            return list.ToArray();
+        }
+        public struct Operator
+        {
+            public char Symbol;
+            public byte Priority;
+        }
+    }
+}
--- Project Visual Studio/Calculation Line/Calculator/Program.cs
+++ Project Visual Studio/Calculation Line/Calculator/Program.cs
@@ -1,42 +1,14 @@
-﻿using Calculator.Calculator;
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Text.RegularExpressions;
+﻿using System;
 
 namespace Calculator
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.Write("Напишите выражение: ");
-            string str = "((5+5)+6+(5+5)+7)";
-            Console.WriteLine();
-            var r = CalculateStr(str);
-
-            Console.ReadKey();
+            Console.WriteLine("\r\n{0}\r\n",Calculator.Calculate(Console.ReadLine()));
+            Main();
         }
-
-        private static double CalculateStr(string str)
-        {
-            Parenthesis.ParenthesisData[] parenthesisDatas = Parenthesis.GetData(str);
-            int[] CountChildrenSort = parenthesisDatas.Select(x => x.CountChildren).GroupBy(x => x).Select(x => x.First()).ToImmutableSortedSet().ToArray();
-            for (int i = 0; i < CountChildrenSort.Length; i++)
-            {
-                int[] vs = parenthesisDatas.Select((x, y) => (x, y)).Where(x => x.x.CountChildren == CountChildrenSort[i]).Select(x => x.y).ToArray();
-                for (int j = 0; j < vs.Length; j++)
-                {
-                    double r = GetResul(str.Substring(parenthesisDatas[i].IndexIn + 1, parenthesisDatas[i].IndexOut - parenthesisDatas[i].IndexIn - 1));
-                }
-            }
-            return 0;
-        }
-        private static double GetResul(string str)
-        {
-            return 0;
-        }
-
     }
 }
