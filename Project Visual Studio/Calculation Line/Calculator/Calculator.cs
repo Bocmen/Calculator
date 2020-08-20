9284626aa2d9e25c4307aa0ed5a40a29fadcfa97
Bocmen <Bocmen@users.noreply.github.com>
2020-08-21 01:08:52 +0300

    Рефакторинг

--- Project Visual Studio/Calculation Line/Calculator/Calculator.cs
+++ Project Visual Studio/Calculation Line/Calculator/Calculator.cs
@@ -7,9 +7,23 @@ namespace Calculator
 {
     public static class Calculator
     {
+        /// <summary>
+        /// Строчное обозначение логарифма OperatorLog(x,y)
+        /// </summary>
         private const string OperatorLog = "log";
-        private const char OperatorParenthesisIn = '(';
-        private const char OperatorParenthesisOut = ')';
+
+        /// <summary>
+        /// Символьные обозначения операторов
+        /// </summary>
+        private const char
+            OperatorParenthesisIn = '(',
+            OperatorParenthesisOut = ')',
+            OperatorLogChar = 'l',
+            OperatorPlus = '+',
+            OperatorMinus = '-',
+            OperatorMultiply = '*',
+            OperatorShare = '/',
+            OperatorPow = '^';
 
 
         /// <summary>
@@ -19,12 +33,12 @@ namespace Calculator
         {
             new Operator { Symbol = OperatorParenthesisIn, Priority = 0 },
             new Operator { Symbol = OperatorParenthesisOut, Priority = 0 },
-            new Operator { Symbol = '+', Priority = 1 },
-            new Operator { Symbol = '-', Priority = 1 },
-            new Operator { Symbol = '/', Priority = 2 },
-            new Operator { Symbol = '*', Priority = 2 },
-            new Operator { Symbol = '^', Priority = 3 },
-            new Operator { Symbol = 'l', Priority = 4 }, // Логарифм
+            new Operator { Symbol = OperatorPlus, Priority = 1 },
+            new Operator { Symbol = OperatorMinus, Priority = 1 },
+            new Operator { Symbol = OperatorShare, Priority = 2 },
+            new Operator { Symbol = OperatorMultiply, Priority = 2 },
+            new Operator { Symbol = OperatorPow, Priority = 3 },
+            new Operator { Symbol = OperatorLogChar, Priority = 4 }, // Логарифм
         };
         /// <summary>
         /// Подсчёт выражения из строки
@@ -32,46 +46,46 @@ namespace Calculator
         /// <returns>Результат выражения</returns>
         public static double Calculate(string str)
         {
-            //try
-            //{
-            object[] Elements = HendlerObject(FilterStr(str));
-            Stack<double> Number = new Stack<double>();
-            Stack<Operator> operators = new Stack<Operator>();
-            foreach (var elem in Elements)
+            try
             {
-                if (elem.GetType() == typeof(double))
+                object[] Elements = HendlerObject(FilterStr(str));
+                Stack<double> Number = new Stack<double>();
+                Stack<Operator> operators = new Stack<Operator>();
+                foreach (var elem in Elements)
                 {
-                    Number.Push((double)elem);
-                }
-                else
-                {
-                    if (operators.Count > 0 && Number.Count > 1 && operators.Peek().Priority != 0 && ((Operator)elem).Priority != 0 && operators.Peek().Priority >= ((Operator)elem).Priority)
+                    if (elem.GetType() == typeof(double))
                     {
-                        Number.Push(CalculateTwoElem(Number.Pop(), Number.Pop(), operators.Pop()));
-                    }
-                    if (((Operator)elem).Priority == 0 && ((Operator)elem).Symbol == ')')
-                    {
-                        while (operators.Peek().Symbol != '(')
-                            Number.Push(CalculateTwoElem(Number.Pop(), Number.Pop(), operators.Pop()));
-                        operators.Pop();
+                        Number.Push((double)elem);
                     }
                     else
-                        operators.Push((Operator)elem);
+                    {
+                        if (operators.Count > 0 && Number.Count > 1 && operators.Peek().Priority != 0 && ((Operator)elem).Priority != 0 && operators.Peek().Priority >= ((Operator)elem).Priority)
+                        {
+                            Number.Push(CalculateTwoElem(Number.Pop(), Number.Pop(), operators.Pop()));
+                        }
+                        if (((Operator)elem).Priority == 0 && ((Operator)elem).Symbol == ')')
+                        {
+                            while (operators.Peek().Symbol != '(')
+                                Number.Push(CalculateTwoElem(Number.Pop(), Number.Pop(), operators.Pop()));
+                            operators.Pop();
+                        }
+                        else
+                            operators.Push((Operator)elem);
+                    }
                 }
+                for (int i = operators.Count; i > 0; i--)
+                    Number.Push(CalculateTwoElem(Number.Pop(), Number.Pop(), operators.Pop()));
+                return Number.Pop();
             }
-            for (int i = operators.Count; i > 0; i--)
-                Number.Push(CalculateTwoElem(Number.Pop(), Number.Pop(), operators.Pop()));
-            return Number.Pop();
-            //}
-            //catch
-            //{
-            //    throw new Exception("Введенное выражение неправильно записано");
-            //}
+            catch
+            {
+                throw new Exception("Введенное выражение неправильно записано");
+            }
         }
         /// <summary>
         /// Удаляет ненужные части (подготавливает строку к разбитию её на числа и операторы)
         /// </summary>
-        private static string FilterStr(string str)=> FilterLog(str.Replace('.', ',').Replace("+-", "-").Replace("-+", "-"));
+        private static string FilterStr(string str) => FilterLog(str.Replace('.', ','));
         /// <summary>
         /// Делает выражение log(X,Y) читабельным для алгоритма
         /// </summary>
@@ -87,7 +101,7 @@ namespace Calculator
                 // Получаем параметры X, Y
                 string[] param = str.Substring(StartIndex, EndIndex - StartIndex).Split(',');
                 // Вставляем исправленое выражение дя подсчёта олгоритмом YlX
-                str = $"{str.Substring(0, matches[i].Index)}({Calculate(param[1])}l{Calculate(param[0])}){str.Substring(EndIndex + 1, str.Length - EndIndex - 1)}";
+                str = $"{str.Substring(0, matches[i].Index)}{OperatorParenthesisIn}{Calculate(param[1])}{OperatorLogChar}{Calculate(param[0])}{OperatorParenthesisOut}{str.Substring(EndIndex + 1, str.Length - EndIndex - 1)}";
                 // Поиск индекса закрывающей скобки
                 int SearchParenthesis(int StartSearchIndex)
                 {
@@ -115,17 +129,17 @@ namespace Calculator
         {
             switch (@operator.Symbol)
             {
-                case '+':
+                case OperatorPlus:
                     return One + Two;
-                case '-':
+                case OperatorMinus:
                     return One - Two;
-                case '*':
+                case OperatorMultiply:
                     return One * Two;
-                case '/':
+                case OperatorShare:
                     return One / Two;
-                case '^':
+                case OperatorPow:
                     return Math.Pow(One, Two);
-                case 'l':
+                case OperatorLogChar:
                     return Math.Log(One, Two);
                 default:
                     return 0;
